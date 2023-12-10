eccceccd12fff4c68140a7134d77d9407008bf0c
aexol <aexol@aexol.com>
2023-12-10 23:47:15 +0100

    fixed productimage

--- src/components/atoms/ProductImage.tsx
+++ src/components/atoms/ProductImage.tsx
@@ -2,9 +2,9 @@ import styled from '@emotion/styled';
 
 export const ProductImage = styled.img<{ size: 'thumbnail' | 'tile' | 'popup' | 'detail' | 'full' }>`
     height: ${p =>
-        p.size === 'tile' ? '36rem' : p.size === 'popup' ? '48rem' : p.size === 'detail' ? '65rem' : '8rem'};
+        p.size === 'tile' ? '36rem' : p.size === 'popup' ? '48rem' : p.size === 'detail' ? '60rem' : '8rem'};
     width: ${p =>
-        p.size === 'tile' ? '24rem' : p.size === 'popup' ? '32rem' : p.size === 'detail' ? '54rem' : '8rem'};
+        p.size === 'tile' ? '24rem' : p.size === 'popup' ? '32rem' : p.size === 'detail' ? '52rem' : '8rem'};
     object-fit: cover;
     flex: 0 0 auto;
 `;

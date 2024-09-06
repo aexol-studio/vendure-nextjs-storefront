79746a3242ebf5ac4582d820d0f623226bd59613
Michal <michal@aexol.com>
2024-09-06 12:44:36 +0200

    ➕ restore placeholder

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -13,6 +13,25 @@ export default class MyDocument extends Document {
             <Html lang={lang}>
                 <Head>
                 <meta name="robots" content="noindex">
+                    {/* enable analytics script only for production */}
+                    {/* {isProduction && (
+                        <>
+                            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+                            <script
+                                // eslint-disable-next-line react/no-danger
+                                dangerouslySetInnerHTML={{
+                                    __html: `
+                                        window.dataLayer = window.dataLayer || [];
+                                        function gtag(){dataLayer.push(arguments);}
+                                        gtag('js', new Date());
+                                        gtag('config', '${GA_TRACKING_ID}', {
+                                          page_path: window.location.pathname,
+                                        });
+                                      `,
+                                }}
+                            />
+                        </>
+                    )} */}
                 </Head>
                 <body>
                     <Main />

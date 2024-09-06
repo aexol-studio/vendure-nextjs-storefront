f2c3ea028fd1b6f4955cb4eaf3470b12109a2300
Michal <michal@aexol.com>
2024-09-06 12:43:00 +0200

    ✏️ remove indexing and crawling

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -12,25 +12,7 @@ export default class MyDocument extends Document {
         return (
             <Html lang={lang}>
                 <Head>
-                    {/* enable analytics script only for production */}
-                    {/* {isProduction && (
-                        <>
-                            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-                            <script
-                                // eslint-disable-next-line react/no-danger
-                                dangerouslySetInnerHTML={{
-                                    __html: `
-                                        window.dataLayer = window.dataLayer || [];
-                                        function gtag(){dataLayer.push(arguments);}
-                                        gtag('js', new Date());
-                                        gtag('config', '${GA_TRACKING_ID}', {
-                                          page_path: window.location.pathname,
-                                        });
-                                      `,
-                                }}
-                            />
-                        </>
-                    )} */}
+                <meta name="robots" content="noindex">
                 </Head>
                 <body>
                     <Main />
